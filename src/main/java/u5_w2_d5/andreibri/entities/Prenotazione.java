3cf404e02edd61d2435165d1d61ca2cb11b165cd
Andrei Briceag <andrei98briceag@gmail.com>
2026-04-17 11:25:17 +0200

    Entities Creation

---
+++ src/main/java/u5_w2_d5/andreibri/entities/Prenotazione.java
@@ -0,0 +1,41 @@
+package u5_w2_d5.andreibri.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+        name = "prenotazioni",
+        // Un dipendente non puó avere piú prenotazioni per lo stesso giono
+        uniqueConstraints = @UniqueConstraint(
+                name = "uk_dipendente_data",
+                columnNames = {"dipendente_id", "data_richiesta"}
+        )
+)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Prenotazione {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Relazione Prenotazione -> Dipendente
+    @ManyToOne
+    @JoinColumn(name = "viaggio_id", nullable = false)
+    private Viaggio viaggio;
+
+    // Relazione Prenotazione -> Dipendente
+    @ManyToOne
+    @JoinColumn(name = "dipendente_id", nullable = false)
+    private Dipendente dipendente;
+
+    @Column(name = "data_richiesta", nullable = false)
+    private String dataRichiesta;
+
+    @Column(columnDefinition = "TEXT")
+    private String note;
+}
